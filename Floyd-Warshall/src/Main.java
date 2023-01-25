fba25e0817230132a9ace97cccd5003ff1ead681
William <willvsouza@gmail.com>
2023-01-25 02:29:40 -0300

    Ajustes no código

--- Floyd-Warshall/src/Main.java
+++ Floyd-Warshall/src/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 class Main
 {
-    // Função recursivo para imprimir o caminho de um determinado vértice `u` do vértice de origem `v`
+    // Função recursiva para imprimir o caminho de um determinado vértice `u` do vértice de origem `v`
     private static void printPath(int[][] path, int v, int u, List<Integer> route)
     {
         if (path[v][u] == v) {
@@ -46,7 +46,7 @@ class Main
         int n = adjMatrix.length;
 
         // cost[] e path[] armazena o caminho mais curto
-        // informações (custo mais curto/rota mais curta)
+        // informações (menor custo)
         int[][] cost = new int[n][n];
         int[][] path = new int[n][n];
 
@@ -93,7 +93,7 @@ class Main
                 // gráfico contém um ciclo de peso negativo
                 if (cost[v][v] < 0)
                 {
-                    System.out.println("Negative-weight cycle found!!");
+                    System.out.println("Ciclo negativo encontrado!!");
                     return;
                 }
             }
